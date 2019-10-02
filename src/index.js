5258d896aa2b423a8ca8f01121a6675585e10636
Ahmed.Ollaek <Ahmed.Ollaek@TECH-OLLEK>
2019-10-02 17:01:13 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/scss/argon-design-system-react.scss";
+
+import HomePage from "views/HomePage";
+import Landing from "views/examples/Landing.jsx";
+import Login from "views/examples/Login.jsx";
+import Profile from "views/examples/Profile.jsx";
+import Register from "views/examples/Register.jsx";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact render={props => <HomePage {...props} />} />
+      <Route
+        path="/landing-page"
+        exact
+        render={props => <Landing {...props} />}
+      />
+      <Route path="/login-page" exact render={props => <Login {...props} />} />
+      <Route
+        path="/profile-page"
+        exact
+        render={props => <Profile {...props} />}
+      />
+      <Route
+        path="/register-page"
+        exact
+        render={props => <Register {...props} />}
+      />
+      <Redirect to="/" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
